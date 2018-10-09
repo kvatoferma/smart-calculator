266d4e82b4f3e5ea8e95bfeeb2df7cee1cc515a0
kvatoferma <jimmaggot@mail.ru>
2018-10-09 16:59:27 +0300

    breaking change: refactor stack algorythm, add operations state objects

--- src/index.js
+++ src/index.js
@@ -1,88 +1,70 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.initialValue = initialValue;
-    this.priority = [];
-    this.values = [];
-    this.operators = [];
-    this.secondaryPriority = [];
-    this.secondaryValues = [];
-    this.secondaryOperators = [];
-    this.values.push(initialValue);
-    this.result = 'hello';
+    this.stack = initialValue;
+    this.previousOperation = {value: null, func: null, priority: null};
+    this.currentOperation = {value: null, func: null, priority: null};
   }
 
-  calculate(count = 0) {
-    if (values.length === 1) {
-      this.result = values[0];
+  previousIsEmpty() {
+    const isNull = value => {
+      return (value === null) || false;
     }
-    const newValues = [];
-    const newOperators = [];
-    const newPriority = [];
-    newValues.push(this.values.unshift());
-    this.priority.forEach((item, i) =>{
-      if (item !== count) {
-        newPriority.push(item);
-        newOperators.push(this.operators[i]);
-      } else {
-
-        newValues[i] = this.operators[i](newValues[i], this.values[i]);
-        newValues;
-      }
-    });
+      let values = Object.values(this.previousOperation);
+      return values.reduce((acc, item) => {
+        return acc && isNull(item);
+      }, true);
+    }
 
 
-    return this.result;
+  calculate() {
+    return this.currentOperation.func(this.stack,this.currentOperation.value);
   }
 
+  calculateWithPriority() {}
 
-// идея:
-// каждый метод операций возвращает обект вида {значения, анонимная функция - оператор, приоритет}
-// функция calculate() получает этот объект и кладет её в стек
-// и если приоритет получаемой операции выше, то она сдигается в очереди к началу
+  checkPriority() {}
 
-
-
-  addOperation(number, operation, priority) {
-    this.values.push(number);
-    this.operators.push(operation);
-    this.priority.push(priority);
+  setOperation(number, func, priority) {
+    this.currentOperation.value = number;
+    this.currentOperation.func = func;
+    this.currentOperation.priority = priority;
   }
 
   add(number) {
-    this.addOperation(number, (a, b) => { return a + b }, 2);
-    // например:
-    // calculate({number, (a, b) => { return a + b}, 2});
-    return this.calculate();
+    this.setOperation(number, (a, b) => { return a + b }, 2);
+    return (this.previousIsEmpty()) ? this.calculate() : this.calculateWithPriority();
   }
 
   subtract(number) {
-    this.addOperation(number, (a, b) => { return a - b }, 2);
-    return this.calculate();
+    this.setOperation(number, (a, b) => { return a - b }, 2);
+    this.calculate();
   }
 
   multiply(number) {
-    this.addOperation(number, (a, b) => { return a * b }, 1);
-    return this.calculate();
+    this.setOperation(number, (a, b) => { return a * b }, 1);
+    this.calculate();
   }
 
   devide(number) {
-    this.addOperation(number, (a, b) => { return a / b }, 1);
-    return this.calculate();
+    this.setOperation(number, (a, b) => { return a / b }, 1);
+    this.calculate();
   }
 
   pow(number) {
-    this.addOperation(number, (a, b) => { return a ** b }, 0);
-    return this.calculate();
+    this.setOperation(number, (a, b) => { return a ** b }, 0);
+    this.calculate();
   }
 }
 
 const calc = new SmartCalculator(20);
+console.log(calc.stack);
 console.log(calc.add(3));
+console.log(calc.currentOperation);
 console.log(calc.subtract(5));
 calc.multiply(10);
 calc.devide(11);
 calc.pow(7);
-console.log(calc.values, calc.operators, calc.priority);
+
 
 
 
